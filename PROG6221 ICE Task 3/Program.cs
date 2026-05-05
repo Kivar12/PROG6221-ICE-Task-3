9e4ec8700de379a23a590b922b061af6be352045
Kivar12 <ST10479337@vcconnect.edu.za>
2026-05-05 19:18:11 +0200

    Add files via upload

---
+++ PROG6221 ICE Task 3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PROG6221_ICE_Task_3
+
+{
+
+    public delegate void AlarmHandler();
+
+    public class Alarm
+    {
+        public event AlarmHandler OnAlarmTriggered;
+
+        public void TriggerAlarm()
+        {
+            Console.WriteLine("Alarm is triggered!");
+
+            OnAlarmTriggered?.Invoke();
+        }
+    }
+
+    class Program
+    {
+        public static void RespondToAlarm()
+        {
+            Console.WriteLine("Warning! Take action immediately!");
+        }
+
+        static void Main(string[] args)
+        {
+
+            Alarm alarm = new Alarm();
+
+            alarm.OnAlarmTriggered += RespondToAlarm;
+
+            alarm.TriggerAlarm();
+        }
+    }
+}
